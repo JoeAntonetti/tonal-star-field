a6e2a8afa4b3203f299f4681837fe6b10aebe1ca
Scott Walters <me@scowalt.com>
2014-07-15 16:05:35 -0500

    Remove old stars for massive performance increase

--- js/main.js
+++ js/main.js
@@ -18,7 +18,17 @@ function render() {
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
 
+	// zoom out camera
 	camera.position.z += 1;
+
+	// remove far away objects from scne
+	scene.children.forEach(function(object){
+		if ((camera.position.z - object.position.z) > FIELD_OF_VIEW) {
+			scene.remove(object);
+		}
+	});
+
+	// add stars to scene
 	for (var i = 0; i < STARS_PER_FRAME; i++) {
 		var circle = new THREE.Mesh(geometry, material);
 		circle.position.set(Math.floor(Math.random() * window.innerWidth) - window.innerWidth / 2, Math.floor(Math.random() * window.innerHeight) - window.innerHeight / 2, camera.position.z);
